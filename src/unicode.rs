ce0db98c3c1f0c5d89bd04317c57bef39a9b2f1b
Randolf Jung <randolf@mathematic.io>
2023-10-21 21:28:58 +0200

    Use ICU compiled data (#38)

--- src/unicode.rs
+++ src/unicode.rs
@@ -1,24 +1,11 @@
-#![allow(clippy::unnecessary_lazy_evaluations)]
-
-#[cfg(feature = "normalization")]
-mod icu {
-    use icu_normalizer::ComposingNormalizer;
-
-    include!(concat!(env!("OUT_DIR"), "/addr_spec_icu/mod.rs"));
-
-    thread_local! {
-        pub static NORMALIZER: ComposingNormalizer = unsafe {
-            ComposingNormalizer::try_new_nfc_unstable(&BakedDataProvider).unwrap_unchecked()
-        };
-    }
-}
-
 #[cfg(feature = "normalization")]
 pub fn normalize<S>(value: S) -> String
 where
     S: AsRef<str>,
 {
-    icu::NORMALIZER.with(|normalizer| normalizer.normalize(value.as_ref()))
+    use icu_normalizer::ComposingNormalizer;
+    const NORMALIZER: ComposingNormalizer = ComposingNormalizer::new_nfc();
+    NORMALIZER.normalize(value.as_ref())
 }
 
 #[cfg(not(feature = "normalization"))]
